b8c6450af8f35770d601785a31e53113c4a5cb5c
Jack <zfccxt@gmail.com>
2022-03-07 15:00:01 +0000

    Switch to new api

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,17 @@ int main() {
 
   cl::MeshCreateInfo mesh_info;
   mesh_info.vertex_input_layout = shader->GetInputLayout();
-  std::vector<float> vertices = {
+  mesh_info.vertices = {
   //  x      y     z   
     -0.5f, -0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
     -0.5f,  0.5f, 0.0f,
      0.5f,  0.5f, 0.0f,
   };
-  std::vector<uint32_t> indices = {
+  mesh_info.indices = {
     0, 1, 2, 
     1, 3, 2, 
   };
-  mesh_info.vertices = vertices.data();
-  mesh_info.num_vertices = vertices.size();
-  mesh_info.indices = indices.data();
-  mesh_info.num_indices = indices.size();
   auto mesh = context->CreateMesh(mesh_info);
 
   glm::vec4 colour(0,0,0,1);
