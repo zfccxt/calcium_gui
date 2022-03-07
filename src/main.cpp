056942eea8975b4999b4fc1a2d20c0307fce8958
Jack <zfccxt@gmail.com>
2022-03-07 14:34:49 +0000

    Test imgui stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <calcium.hpp>
 #include <clx_imgui.hpp>
 #include <clx_imgui_vulkan.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 int main() {
   cl::ContextCreateInfo context_info;
@@ -8,7 +10,9 @@ int main() {
   context_info.extensions = { clx::imgui::LoadExtension() };
   auto context = cl::Context::CreateContext(context_info);
 
-  auto window = context->CreateWindow();
+  cl::WindowCreateInfo window_info;
+  window_info.enable_vsync;
+  auto window = context->CreateWindow(window_info);
 
   auto shader = context->CreateShader("res/shaders/shader.vert.spv", "res/shaders/shader.frag.spv");
 
@@ -31,17 +35,28 @@ int main() {
   mesh_info.num_indices = indices.size();
   auto mesh = context->CreateMesh(mesh_info);
 
+  glm::vec4 colour(0,0,0,1);
+
   while (window->IsOpen()) {
     window->PollEvents();
     
     context->BeginRenderPass();
+      window->SetClearColour({colour.r, colour.g, colour.b, colour.a});
 
-    shader->Bind();
-    mesh->Draw();
+        shader->Bind();
+        mesh->Draw();
 
-    clx::imgui::Begin();
-    ImGui::ShowDemoWindow();
-    clx::imgui::End();
+      clx::imgui::Begin();
+
+        ImGui::Begin("background colour picker");
+          ImGui::ColorEdit4("BG Col", glm::value_ptr(colour));
+          ImGui::Text("hello");
+          ImGui::Button("button");
+        ImGui::End();
+
+        ImGui::ShowDemoWindow();
+
+      clx::imgui::End();
 
     context->EndRenderPass();
 
